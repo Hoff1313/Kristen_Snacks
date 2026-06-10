59b41a2b892a9bce809e68c9b63296a352dea4b6
Hoff1313 <udontknowjack39289@gmail.com>
2026-06-10 12:33:27 -0500

    Update data.js

--- data.js
+++ data.js
@@ -1,127 +1,33 @@
 window.SNACKS = [
 
-  {
-    id: 1,
-    name: "Chocolate Covered Bacon",
-    park: "Resort / Polynesian",
-    location: "Kona Island",
-    image: "snack_01.png",
-    tried: false,
-    disney: "https://disneyworld.disney.go.com/maps/?search=Kona+Island",
-    google: "https://www.google.com/maps/search/?api=1&query=Kona+Island+Polynesian+Resort"
-  },
+{ id: 1, name: "Chocolate Covered Bacon", park: "Resort / Polynesian", location: "Kona Island", image: "<img src='snack_01.png'>", tried: false, disney: "https://disneyworld.disney.go.com/maps/?search=Kona+Island", google: "https://www.google.com/maps/search/?api=1&query=Kona+Island+Disney" },
 
-  {
-    id: 2,
-    name: "Crème Brulee Croissant",
-    park: "Magic Kingdom",
-    location: "Gaston’s Tavern",
-    image: "snack_02.png",
-    tried: false,
-    disney: "https://disneyworld.disney.go.com/maps/?search=Gastons+Tavern",
-    google: "https://www.google.com/maps/search/?api=1&query=Gastons+Tavern+Magic+Kingdom"
-  },
+{ id: 2, name: "Creme Brulee Croissant", park: "Magic Kingdom", location: "Gastons Tavern", image: "<img src='snack_02.png'>", tried: false, disney: "https://disneyworld.disney.go.com/maps/?search=Gastons+Tavern", google: "https://www.google.com/maps/search/?api=1&query=Gastons+Tavern" },
 
-  {
-    id: 3,
-    name: "Brioche Ice Cream Sandwich",
-    park: "EPCOT",
-    location: "L’Artisan des Glaces",
-    image: "snack_03.png",
-    tried: false,
-    disney: "https://disneyworld.disney.go.com/maps/?search=LArtisan+des+Glaces",
-    google: "https://www.google.com/maps/search/?api=1&query=LArtisan+des+Glaces+EPCOT"
-  },
+{ id: 3, name: "Brioche Ice Cream Sandwich", park: "EPCOT", location: "Artisan des Glaces", image: "<img src='snack_03.png'>", tried: false, disney: "https://disneyworld.disney.go.com/maps/?search=LArtisan+des+Glaces", google: "https://www.google.com/maps/search/?api=1&query=Artisan+des+Glaces+EPCOT" },
 
-  {
-    id: 4,
-    name: "Mickey Beignet Sundae",
-    park: "Resort / Port Orleans",
-    location: "Scat Cat’s Café",
-    image: "snack_04.png",
-    tried: false,
-    disney: "https://disneyworld.disney.go.com/maps/?search=Scat+Cats+Cafe",
-    google: "https://www.google.com/maps/search/?api=1&query=Scat+Cats+Cafe+Disney"
-  },
+{ id: 4, name: "Mickey Beignet Sundae", park: "Resort", location: "Scat Cats Cafe", image: "<img src='snack_04.png'>", tried: false, disney: "https://disneyworld.disney.go.com/maps/?search=Scat+Cats+Cafe", google: "https://www.google.com/maps/search/?api=1&query=Scat+Cats+Cafe" },
 
-  {
-    id: 5,
-    name: "Churro Sundae",
-    park: "EPCOT",
-    location: "La Cantina de San Angel",
-    image: "snack_05.png",
-    tried: false,
-    disney: "https://disneyworld.disney.go.com/maps/?search=La+Cantina+de+San+Angel",
-    google: "https://www.google.com/maps/search/?api=1&query=La+Cantina+de+San+Angel"
-  },
+{ id: 5, name: "Churro Sundae", park: "EPCOT", location: "La Cantina de San Angel", image: "<img src='snack_05.png'>", tried: false, disney: "https://disneyworld.disney.go.com/maps/?search=La+Cantina+de+San+Angel", google: "https://www.google.com/maps/search/?api=1&query=La+Cantina+de+San+Angel" },
 
-  {
-    id: 6,
-    name: "Cookie Dough Ice Cream Sandwich",
-    park: "Animal Kingdom",
-    location: "Terra Treats",
-    image: "snack_06.png",
-    tried: false,
-    disney: "https://disneyworld.disney.go.com/maps/?search=Terra+Treats",
-    google: "https://www.google.com/maps/search/?api=1&query=Terra+Treats+Animal+Kingdom"
-  },
+{ id: 6, name: "Cookie Dough Ice Cream Sandwich", park: "Animal Kingdom", location: "Terra Treats", image: "<img src='snack_06.png'>", tried: false, disney: "https://disneyworld.disney.go.com/maps/?search=Terra+Treats", google: "https://www.google.com/maps/search/?api=1&query=Terra+Treats" },
 
-  {
-    id: 7,
-    name: "Lunch Box Tart",
-    park: "Hollywood Studios",
-    location: "Woody’s Lunch Box",
-    image: "snack_07.png",
-    tried: false,
-    disney: "https://disneyworld.disney.go.com/maps/?search=Woodsys+Lunch+Box",
-    google: "https://www.google.com/maps/search/?api=1&query=Wodys+Lunch+Box+Hollywood+Studios"
-  },
+{ id: 7, name: "Lunch Box Tart", park: "Hollywood Studios", location: "Woodys Lunch Box", image: "<img src='snack_07.png'>", tried: false, disney: "https://disneyworld.disney.go.com/maps/?search=Woodys+Lunch+Box", google: "https://www.google.com/maps/search/?api=1&query=Woodys+Lunch+Box" },
 
-  {
-    id: 8,
-    name: "Grilled Cheese",
-    park: "Hollywood Studios",
-    location: "Woody’s Lunch Box",
-    image: "snack_08.png",
-    tried: false,
-    disney: "https://disneyworld.disney.go.com/maps/?search=Woodsys+Lunch+Box",
-    google: "https://www.google.com/maps/search/?api=1&query=Wodys+Lunch+Box"
-  },
+{ id: 8, name: "Grilled Cheese", park: "Hollywood Studios", location: "Woodys Lunch Box", image: "<img src='snack_08.png'>", tried: false, disney: "https://disneyworld.disney.go.com/maps/?search=Woodys+Lunch+Box", google: "https://www.google.com/maps/search/?api=1&query=Woodys+Lunch+Box" },
 
-  {
-    id: 9,
-    name: "Foot Long Mozzarella Stick",
-    park: "Magic Kingdom",
-    location: "Tony’s Town Square",
-    image: "snack_09.png",
-    tried: false,
-    disney: "https://disneyworld.disney.go.com/maps/?search=Tonys+Town+Square",
-    google: "https://www.google.com/maps/search/?api=1&query=Tonys+Town+Square+Magic+Kingdom"
-  },
+{ id: 9, name: "Mozzarella Stick", park: "Magic Kingdom", location: "Tonys Town Square", image: "<img src='snack_09.png'>", tried: false, disney: "https://disneyworld.disney.go.com/maps/?search=Tonys+Town+Square", google: "https://www.google.com/maps/search/?api=1&query=Tonys+Town+Square" },
 
-  {
-    id: 10,
-    name: "Sweet and Spicy Waffle",
-    park: "Magic Kingdom",
-    location: "Sleepy Hollow",
-    image: "snack_10.png",
-    tried: false,
-    disney: "https://disneyworld.disney.go.com/maps/?search=Sleepy+Hollow",
-    google: "https://www.google.com/maps/search/?api=1&query=Sleepy+Hollow+Magic+Kingdom"
-  },
+{ id: 10, name: "Sweet Spicy Waffle", park: "Magic Kingdom", location: "Sleepy Hollow", image: "<img src='snack_10.png'>", tried: false, disney: "https://disneyworld.disney.go.com/maps/?search=Sleepy+Hollow", google: "https://www.google.com/maps/search/?api=1&query=Sleepy+Hollow" },
 
-  // ✅ Skipping repeating pattern explanation — rest follow same clean format
+{ id: 11, name: "Candied Bacon", park: "Magic Kingdom", location: "Golden Oak Outpost", image: "<img src='snack_11.png'>", tried: false, disney: "https://disneyworld.disney.go.com/maps/?search=Golden+Oak+Outpost", google: "https://www.google.com/maps/search/?api=1&query=Golden+Oak+Outpost" },
 
-  {
-    id: 33,
-    name: "Chicken Goof-ups",
-    park: "EPCOT",
-    location: "Grab-N-Goof",
-    image: "snack_34.png",
-    tried: false,
-    disney: "https://disneyworld.disney.go.com/maps/?search=Grab-N-Goof",
-    google: "https://www.google.com/maps/search/?api=1&query=Grab-N-Goof+EPCOT"
-  }
+{ id: 12, name: "PBJ Croissant", park: "Resort", location: "Pop Century", image: "<img src='snack_12.png'>", tried: false, disney: "https://disneyworld.disney.go.com/maps/?search=Pop+Century", google: "https://www.google.com/maps/search/?api=1&query=Pop+Century" },
+
+{ id: 13, name: "Pretzel", park: "Disney Springs", location: "Wetzels Pretzels", image: "<img src='snack_13.png'>", tried: false, disney: "https://disneyworld.disney.go.com/maps/?search=Wetzels+Pretzels", google: "https://www.google.com/maps/search/?api=1&query=Wetzels+Pretzels" },
+
+{ id: 14, name: "Mac and Pork", park: "Animal Kingdom", location: "Flame Tree BBQ", image: "<img src='snack_14.png'>", tried: false, disney: "https://disneyworld.disney.go.com/maps/?search=Flame+Tree+Barbecue", google: "https://www.google.com/maps/search/?api=1&query=Flame+Tree+BBQ" },
+
+{ id: 33, name: "Chicken Goof ups", park: "EPCOT", location: "Grab N Goof", image: "<img src='snack_34.png'>", tried: false, disney: "https://disneyworld.disney.go.com/maps/?search=Grab+N+Goof", google: "https://www.google.com/maps/search/?api=1&query=Grab+N+Goof" }
 
 ];
-``
