f84b1de14bb7044bf427113e7e78e23939f22740
Hoff1313 <udontknowjack39289@gmail.com>
2026-06-10 12:37:52 -0500

    Update data.js

--- data.js
+++ data.js
@@ -1,70 +1,37 @@
-
 window.SNACKS = [
 
-{ id: 1, name: "Chocolate Covered Bacon", park: "Resort", location: "Kona Island", image: "<img src='snack_01.png'>", tried: false, disney: "https://disneyworld.disney.go.com/maps/?search=Kona+Island", google: "https://www.google.com/maps/search/?api=1&query=Kona+Island+Disney" },
-
-{ id: 2, name: "Creme Brulee Croissant", park: "Magic Kingdom", location: "Gastons Tavern", image: "<img src='snack_02.png'>", tried: false, disney: "https://disneyworld.disney.go.com/maps/?search=Gastons+Tavern", google: "https://www.google.com/maps/search/?api=1&query=Gastons+Tavern+Magic+Kingdom" },
-
-{ id: 3, name: "Brioche Ice Cream Sandwich", park: "EPCOT", location: "Artisan des Glaces", image: "<img src='snack_03.png'>", tried: false, disney: "https://disneyworld.disney.go.com/maps/?search=LArtisan+des+Glaces", google: "https://www.google.com/maps/search/?api=1&query=Artisan+des+Glaces+EPCOT" },
-
-{ id: 4, name: "Mickey Beignet Sundae", park: "Resort", location: "Scat Cats Cafe", image: "<img src='snack_04.png'>", tried: false, disney: "https://disneyworld.disney.go.com/maps/?search=Scat+Cats+Cafe", google: "https://www.google.com/maps/search/?api=1&query=Scat+Cats+Cafe" },
-
-{ id: 5, name: "Churro Sundae", park: "EPCOT", location: "La Cantina de San Angel", image: "<img src='snack_05.png'>", tried: false, disney: "https://disneyworld.disney.go.com/maps/?search=La+Cantina+de+San+Angel", google: "https://www.google.com/maps/search/?api=1&query=La+Cantina+de+San+Angel+EPCOT" },
-
-{ id: 6, name: "Cookie Dough Ice Cream Sandwich", park: "Animal Kingdom", location: "Terra Treats", image: "<img src='snack_06.png'>", tried: false, disney: "https://disneyworld.disney.go.com/maps/?search=Terra+Treats", google: "https://www.google.com/maps/search/?api=1&query=Terra+Treats" },
-
-{ id: 7, name: "Lunch Box Tart", park: "Hollywood Studios", location: "Woodys Lunch Box", image: "<img src='snack_07.png'>", tried: false, disney: "https://disneyworld.disney.go.com/maps/?search=Woodys+Lunch+Box", google: "https://www.google.com/maps/search/?api=1&query=Woodys+Lunch+Box" },
-
-{ id: 8, name: "Grilled Cheese", park: "Hollywood Studios", location: "Woodys Lunch Box", image: "<img src='snack_08.png'>", tried: false, disney: "https://disneyworld.disney.go.com/maps/?search=Woodys+Lunch+Box", google: "https://www.google.com/maps/search/?api=1&query=Woodys+Lunch+Box" },
-
-{ id: 9, name: "Mozzarella Stick", park: "Magic Kingdom", location: "Tonys Town Square", image: "<img src='snack_09.png'>", tried: false, disney: "https://disneyworld.disney.go.com/maps/?search=Tonys+Town+Square", google: "https://www.google.com/maps/search/?api=1&query=Tonys+Town+Square" },
-
-{ id: 10, name: "Sweet Spicy Waffle", park: "Magic Kingdom", location: "Sleepy Hollow", image: "<img src='snack_10.png'>", tried: false, disney: "https://disneyworld.disney.go.com/maps/?search=Sleepy+Hollow", google: "https://www.google.com/maps/search/?api=1&query=Sleepy+Hollow" },
-
-{ id: 11, name: "Candied Bacon", park: "Magic Kingdom", location: "Golden Oak Outpost", image: "<img src='snack_11.png'>", tried: false, disney: "https://disneyworld.disney.go.com/maps/?search=Golden+Oak+Outpost", google: "https://www.google.com/maps/search/?api=1&query=Golden+Oak+Outpost" },
-
-{ id: 12, name: "PBJ Croissant", park: "Resort", location: "Pop Century", image: "<img src='snack_12.png'>", tried: false },
-
-{ id: 13, name: "Pretzel", park: "Disney Springs", location: "Wetzels Pretzels", image: "<img src='snack_13.png'>", tried: false },
-
-{ id: 14, name: "Mac and Pork", park: "Animal Kingdom", location: "Flame Tree BBQ", image: "<img src='snack_14.png'>", tried: false },
-
-{ id: 15, name: "Chicken and Waffles", park: "Resort", location: "Topolinos Terrace", image: "<img src='snack_15.png'>", tried: false },
-
-{ id: 16, name: "Cookie Dough Croissant", park: "Resort", location: "Contempo Cafe", image: "<img src='snack_16.png'>", tried: false },
-
-{ id: 17, name: "Strawberry Mickey Treat", park: "Animal Kingdom", location: "Zuris Sweets Shop", image: "<img src='snack_17.png'>", tried: false },
-
-{ id: 18, name: "Hawaiian Roll", park: "BoardWalk", location: "Blue Ribbon Corn Dogs", image: "<img src='snack_18.png'>", tried: false },
-
-{ id: 19, name: "Caramel Apple Donuts", park: "Magic Kingdom", location: "Friars Nook", image: "<img src='snack_19.png'>", tried: false },
-
-{ id: 20, name: "Lemon Bolle", park: "EPCOT", location: "Norway Beer Cart", image: "<img src='snack_20.png'>", tried: false },
-
-{ id: 21, name: "Lemon Macaron", park: "EPCOT", location: "Sunshine Seasons", image: "<img src='snack_21.png'>", tried: false },
-
-{ id: 22, name: "Cheddar Bacon Tots", park: "Magic Kingdom", location: "Friars Nook", image: "<img src='snack_22.png'>", tried: false },
-
-{ id: 23, name: "Ham Sandwich", park: "Magic Kingdom", location: "Gastons Tavern", image: "<img src='snack_23.png'>", tried: false },
-
-{ id: 24, name: "Mickey Waffles Bacon", park: "Hollywood Studios", location: "Fairfax Fare", image: "<img src='snack_24.png'>", tried: false },
-
-{ id: 25, name: "Ronto Wrap", park: "Hollywood Studios", location: "Ronto Roasters", image: "<img src='snack_25.png'>", tried: false },
-
-{ id: 26, name: "Stuffed Mickey Waffles", park: "Resort", location: "Grand Floridian Cafe", image: "<img src='snack_26.png'>", tried: false },
-
-{ id: 27, name: "Smores Bar", park: "EPCOT", location: "Regal Eagle", image: "<img src='snack_27.png'>", tried: false },
-
-{ id: 28, name: "Strawberry Cookie Sandwich", park: "Magic Kingdom", location: "Big Top Souvenirs", image: "<img src='snack_28.png'>", tried: false },
-
-{ id: 29, name: "Donut Skewers", park: "Disney Springs", location: "Everglazed", image: "<img src='snack_29.png'>", tried: false },
-
-{ id: 30, name: "Vanilla Wafer Sandwich", park: "EPCOT", location: "Mitsukoshi", image: "<img src='snack_30.png'>", tried: false },
-
-{ id: 31, name: "Fairy Bread Cake", park: "Animal Kingdom", location: "Pizzafari", image: "<img src='snack_31.png'>", tried: false },
-
-{ id: 32, name: "Pretzel Bites", park: "Animal Kingdom", location: "Isle of Java", image: "<img src='snack_32.png'>", tried: false },
-
-{ id: 33, name: "Chicken Goof Ups", park: "EPCOT", location: "Grab N Goof", image: "<img src='snack_34.png'>", tried: false }
+{ id: 1, name: "Chocolate Covered Bacon", park: "Resort", location: "Kona Island", image: "snack_01.png", tried: false },
+{ id: 2, name: "Creme Brulee Croissant", park: "Magic Kingdom", location: "Gastons Tavern", image: "snack_02.png", tried: false },
+{ id: 3, name: "Brioche Ice Cream Sandwich", park: "EPCOT", location: "Artisan des Glaces", image: "snack_03.png", tried: false },
+{ id: 4, name: "Mickey Beignet Sundae", park: "Resort", location: "Scat Cats Cafe", image: "snack_04.png", tried: false },
+{ id: 5, name: "Churro Sundae", park: "EPCOT", location: "La Cantina de San Angel", image: "snack_05.png", tried: false },
+{ id: 6, name: "Cookie Dough Ice Cream Sandwich", park: "Animal Kingdom", location: "Terra Treats", image: "snack_06.png", tried: false },
+{ id: 7, name: "Lunch Box Tart", park: "Hollywood Studios", location: "Woodys Lunch Box", image: "snack_07.png", tried: false },
+{ id: 8, name: "Grilled Cheese", park: "Hollywood Studios", location: "Woodys Lunch Box", image: "snack_08.png", tried: false },
+{ id: 9, name: "Mozzarella Stick", park: "Magic Kingdom", location: "Tonys Town Square", image: "snack_09.png", tried: false },
+{ id: 10, name: "Sweet Spicy Waffle", park: "Magic Kingdom", location: "Sleepy Hollow", image: "snack_10.png", tried: false },
+{ id: 11, name: "Candied Bacon", park: "Magic Kingdom", location: "Golden Oak Outpost", image: "snack_11.png", tried: false },
+{ id: 12, name: "PBJ Croissant", park: "Resort", location: "Pop Century", image: "snack_12.png", tried: false },
+{ id: 13, name: "Pretzel", park: "Disney Springs", location: "Wetzels Pretzels", image: "snack_13.png", tried: false },
+{ id: 14, name: "Mac and Pork", park: "Animal Kingdom", location: "Flame Tree BBQ", image: "snack_14.png", tried: false },
+{ id: 15, name: "Chicken and Waffles", park: "Resort", location: "Topolinos Terrace", image: "snack_15.png", tried: false },
+{ id: 16, name: "Cookie Dough Croissant", park: "Resort", location: "Contempo Cafe", image: "snack_16.png", tried: false },
+{ id: 17, name: "Strawberry Mickey Treat", park: "Animal Kingdom", location: "Zuris Sweets Shop", image: "snack_17.png", tried: false },
+{ id: 18, name: "Hawaiian Roll", park: "BoardWalk", location: "Blue Ribbon Corn Dogs", image: "snack_18.png", tried: false },
+{ id: 19, name: "Caramel Apple Donuts", park: "Magic Kingdom", location: "Friars Nook", image: "snack_19.png", tried: false },
+{ id: 20, name: "Lemon Bolle", park: "EPCOT", location: "Norway Beer Cart", image: "snack_20.png", tried: false },
+{ id: 21, name: "Lemon Macaron", park: "EPCOT", location: "Sunshine Seasons", image: "snack_21.png", tried: false },
+{ id: 22, name: "Cheddar Bacon Tots", park: "Magic Kingdom", location: "Friars Nook", image: "snack_22.png", tried: false },
+{ id: 23, name: "Ham Sandwich", park: "Magic Kingdom", location: "Gastons Tavern", image: "snack_23.png", tried: false },
+{ id: 24, name: "Mickey Waffles Bacon", park: "Hollywood Studios", location: "Fairfax Fare", image: "snack_24.png", tried: false },
+{ id: 25, name: "Ronto Wrap", park: "Hollywood Studios", location: "Ronto Roasters", image: "snack_25.png", tried: false },
+{ id: 26, name: "Stuffed Mickey Waffles", park: "Resort", location: "Grand Floridian Cafe", image: "snack_26.png", tried: false },
+{ id: 27, name: "Smores Bar", park: "EPCOT", location: "Regal Eagle", image: "snack_27.png", tried: false },
+{ id: 28, name: "Strawberry Cookie Sandwich", park: "Magic Kingdom", location: "Big Top Souvenirs", image: "snack_28.png", tried: false },
+{ id: 29, name: "Donut Skewers", park: "Disney Springs", location: "Everglazed", image: "snack_29.png", tried: false },
+{ id: 30, name: "Vanilla Wafer Sandwich", park: "EPCOT", location: "Mitsukoshi", image: "snack_30.png", tried: false },
+{ id: 31, name: "Fairy Bread Cake", park: "Animal Kingdom", location: "Pizzafari", image: "snack_31.png", tried: false },
+{ id: 32, name: "Pretzel Bites", park: "Animal Kingdom", location: "Isle of Java", image: "snack_32.png", tried: false },
+{ id: 33, name: "Chicken Goof Ups", park: "EPCOT", location: "Grab N Goof", image: "snack_33.png", tried: false }
 
 ];
