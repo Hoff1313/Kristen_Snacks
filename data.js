bbfc114b0eac51cff64fc56f95b1bbd6cd5606d9
Hoff1313 <udontknowjack39289@gmail.com>
2026-06-10 11:49:58 -0500

    Update data.js

--- data.js
+++ data.js
@@ -31,5 +31,5 @@ window.SNACKS = [
   { id: 30, name: "Frozen Vanilla Wafer Sandwich", park: "EPCOT", location: "Mitsukoshi (Japan Pavilion)", image: "snack_31.png", tried: false },
   { id: 31, name: "Fairy Bread Cake", park: "Animal Kingdom", location: "Pizzafari", image: "snack_32.png", tried: false },
   { id: 32, name: "Pretzel bites", park: "Animal Kingdom", location: "Isle of Java", image: "snack_33.png", tried: false },
-  { id: 33, name: "Chicken Goof-ups", park: "Epcot", location: "Grab-N-Goof", image: "snack_34.png", tried: false }
+  { id: 33, name: "Chicken Goof-ups", park: "EPCOT", location: "Grab-N-Goof", image: "snack_34.png", tried: false }
 ];
